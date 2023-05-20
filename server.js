2ee38467cd853a1f6910077382a2739d3c2548dd
rachelf27 <rachelvfmurphy@gmail.com>
2023-05-19 17:06:37 -0700

    changed filenames to use with beanstalk

--- server.js
+++ server.js
@@ -1,36 +1,33 @@
+// import the required modules and setup server
 const express = require('express');
 const { dirname } = require('path');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000; // use the port provided by the environment variable or default to 8000 locally
 
+// create a route for the application
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/server.html');
 });
 
-// app.listen(8000, () => {
-//     console.log('Server is listening on port 8000');
-// });
-  
+// listen for an incoming socket connections
 io.on('connection', (socket) => {
-    console.log('Connected');
-    io.emit("User connected");
-    
+    console.log('User Connected');
+    // disconnect event
+    socket.on('disconnect', () => {
+        console.log('User Disconnected');
+      });
+    // emit a message event to all connected clients
     socket.on('message', (data) => {
         console.log('Your message:', data);
         io.emit('message', data);
-    });
+    });   
 });
 
+// start the the server to listen by passing in the port number
 http.listen(port, () => {
     console.log(`Server listening on port ${port}`);
-});
-
-io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
-
-io.on('connection', (socket) => {
-    socket.broadcast.emit('hi');
 });
 
 
